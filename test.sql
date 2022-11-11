68a2d69f707224d051bfb1b643d6e2ae9636d2eb
StollARG <stoll.ads@gmail.com>
2022-11-11 14:33:57 -0300

    Actualización de BD

--- test.sql
+++ test.sql
@@ -1,25 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.1.3
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 08-11-2022 a las 20:22:08
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.4
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
---
--- Base de datos: `test`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `producto`
---
 
 CREATE TABLE `producto` (
   `id` bigint(10) NOT NULL,
@@ -28,11 +9,8 @@ CREATE TABLE `producto` (
   `precio` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `stock`
---
+INSERT INTO `producto` (`id`, `id_tipo_producto`, `nombre`, `precio`) VALUES
+(1, 1, 'productoPrueba', 100);
 
 CREATE TABLE `stock` (
   `id` bigint(20) NOT NULL,
@@ -40,76 +18,36 @@ CREATE TABLE `stock` (
   `cantidad` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tipo_producto`
---
+INSERT INTO `stock` (`id`, `id_producto`, `cantidad`) VALUES
+(1, 1, 50);
 
 CREATE TABLE `tipo_producto` (
   `id` bigint(10) NOT NULL,
   `descripcion` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Índices para tablas volcadas
---
+INSERT INTO `tipo_producto` (`id`, `descripcion`) VALUES
+(1, 'barato'),
+(2, 'caro');
 
---
--- Indices de la tabla `producto`
---
+
 ALTER TABLE `producto`
   ADD PRIMARY KEY (`id`),
   ADD KEY `FK_tabla_producto_idTipoProducto` (`id_tipo_producto`);
 
---
--- Indices de la tabla `stock`
---
 ALTER TABLE `stock`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_tabla_stock_id_producto` (`id_producto`);
+  ADD KEY `fk_stock_producto` (`id_producto`);
 
---
--- Indices de la tabla `tipo_producto`
---
 ALTER TABLE `tipo_producto`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
 
---
--- AUTO_INCREMENT de la tabla `producto`
---
 ALTER TABLE `producto`
-  MODIFY `id` bigint(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT de la tabla `stock`
---
 ALTER TABLE `stock`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT de la tabla `tipo_producto`
---
 ALTER TABLE `tipo_producto`
-  MODIFY `id` bigint(10) NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `producto`
---
-ALTER TABLE `producto`
-  ADD CONSTRAINT `FK_tabla_producto_idTipoProducto` FOREIGN KEY (`id_tipo_producto`) REFERENCES `tipo_producto` (`id`);
-
---
--- Filtros para la tabla `stock`
---
-ALTER TABLE `stock`
-  ADD CONSTRAINT `FK_tabla_stock_id_producto` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id`);
-COMMIT;
+  MODIFY `id` bigint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
